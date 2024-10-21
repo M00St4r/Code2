6b7466e087df683e27bd10871371892352a9edba
M00St4r <bjoern.martens@posteo.de>
2024-10-21 11:29:42 +0200

    waaaa

--- LinterTest/LinterTest.js
+++ LinterTest/LinterTest.js
@@ -1,17 +1,18 @@
+"use strict";
 var linterTest;
 (function (linterTest) {
-    var info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
-    console.log(DeCrypt(info.text, info.key));
-    function DeCrypt(text, _Key) {
-        var result = "";
-        for (var i = 0; i < text.length(); i + +)
-            ;
-        result += String.fromCharCode(text.charCodeAt(i) + _Key);
-        return result;
-    }
-    var key;
+    let key;
     (function (key) {
         key[key["Pos"] = 1] = "Pos";
         key[key["neg"] = -1] = "neg";
     })(key || (key = {}));
+    let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
+    console.log(DeCrypt(info.text, info.key));
+    function DeCrypt(text, _Key) {
+        let result = "";
+        for (let i = 0; i < text.length; i++) {
+            result += String.fromCharCode(text.charCodeAt(i) + _Key);
+        }
+        return result;
+    }
 })(linterTest || (linterTest = {}));
