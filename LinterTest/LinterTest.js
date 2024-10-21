78fb6dc8f5bf967099b51876ee631b2c40b772f8
M00St4r <bjoern.martens@posteo.de>
2024-10-21 12:12:56 +0200

    sawfawf

--- LinterTest/LinterTest.js
+++ LinterTest/LinterTest.js
@@ -1,15 +1,16 @@
+"use strict";
 var LinterTest;
 (function (LinterTest) {
-    var KEY;
+    let KEY;
     (function (KEY) {
         KEY[KEY["POS"] = 1] = "POS";
         KEY[KEY["NEG"] = -1] = "NEG";
     })(KEY || (KEY = {}));
-    var info = { text: "G`udetmvhsgBncd1 ", KEY: KEY.POS };
+    const info = { text: "G`udetmvhsgBncd1 ", KEY: KEY.POS };
     console.log(deCrypt(info.text, info.KEY));
     function deCrypt(_text, _key) {
-        var result = "";
-        for (var i = 0; i < _text.length; i++) {
+        let result = "";
+        for (let i = 0; i < _text.length; i++) {
             result += String.fromCharCode(_text.charCodeAt(i) + _key);
         }
         return result;
