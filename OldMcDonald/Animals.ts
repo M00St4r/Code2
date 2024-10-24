6566dbe11823d22f21a1affacccf713f335871aa
M00St4r <bjoern.martens@posteo.de>
2024-10-24 18:14:20 +0200

    idk

--- OldMcDonald/Animals.ts
+++ OldMcDonald/Animals.ts
@@ -1,28 +1,30 @@
-class Animal {
-    type: string;
-    food: number;
-    noise: string;
+namespace OldMcDoof {
+    export class Animal {
+        type: string;
+        food: number;
+        noise: string;
 
-    constructor(_type: string, _food: number, _noise: string) {
-        this.set(_type, _food, _noise);
-    }
+        constructor(_type: string, _food: number, _noise: string) {
+            this.set(_type, _food, _noise);
+        }
 
-    set(_type: string, _food: number, _noise: string) {
-        this.type = _type;
-        this.food = _food;
-        this.noise = _noise;
-    }
+        set(_type: string, _food: number, _noise: string) {
+            this.type = _type;
+            this.food = _food;
+            this.noise = _noise;
+        }
 
-    eat(): void {
-        // if (storage >= this.food) {
-        //     storage -= this.food;
-        //     console.log(storage);
-        // } else {
-        //     alert("no more food left!!!");
-        // }
-    }
+        eat(): void {
+            // if (storage >= this.food) {
+            //     storage -= this.food;
+            //     console.log(storage);
+            // } else {
+            //     alert("no more food left!!!");
+            // }
+        }
 
-    alert(): void {
-        console.log(this.noise);
+        alert(): void {
+            console.log(this.noise);
+        }
     }
 }
